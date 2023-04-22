15651d304e768e2dfecb813aa4c7f319c3532716
league <league@imac-14.attlocal.net>
2023-04-21 18:16:04 -0700

    Harry Potter

--- src/_04_harry_potter/HarryRunner.java
+++ src/_04_harry_potter/HarryRunner.java
@@ -2,6 +2,10 @@ package _04_harry_potter;
 
 public class HarryRunner {
 public static void main(String[] args) {
-	
+	HarryPotter Harry = new HarryPotter();
+	Harry.makeInvisible(true);
+	Harry.spyOnSnape();
+	Harry.makeInvisible(false);
+	Harry.castSpell("stupefy");
 }
 }
